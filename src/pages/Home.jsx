91df106fa71acfc1d143c70b6c4b5e5e56d486c2
Sameer-tech-web <itss52608@gmail.com>
2026-08-15 05:54:25 -0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,47 @@
+import Hero from '../components/Hero';
+import CategoryGrid from '../components/CategoryGrid';
+import AboutSection from '../components/AboutSection';
+import ProductDetailSection from '../components/ProductDetailSection';
+import QualitySection from '../components/QualitySection';
+import SustainabilitySection from '../components/SustainabilitySection';
+import ComplianceSection from '../components/ComplianceSection';
+import CatalogSection from '../components/CatalogSection';
+import ContactSection from '../components/ContactSection';
 
+export default function Home() {
+  return (
+    <>
+      <section id="top">
+        <Hero />
+      </section>
+
+      <section id="collections">
+        <CategoryGrid />
+      </section>
+
+      <section id="about">
+        <AboutSection />
+      </section>
+
+      <ProductDetailSection />
+
+      <section id="quality">
+        <QualitySection />
+      </section>
+
+      <section id="sustainability">
+        <SustainabilitySection />
+      </section>
+
+      <section id="compliance">
+        <ComplianceSection />
+      </section>
+
+      <CatalogSection />
+
+      <section id="contact">
+        <ContactSection />
+      </section>
+    </>
+  );
+}
