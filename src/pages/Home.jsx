3139f8dfa83e013d5b3ab94957a78b3fa0e189af
Sameer-tech-web <itss52608@gmail.com>
2026-08-17 02:26:26 -0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,37 +11,15 @@ import ContactSection from '../components/ContactSection';
 export default function Home() {
   return (
     <>
-      <section id="top">
-        <Hero />
-      </section>
-
-      <section id="collections">
-        <CategoryGrid />
-      </section>
-
-      <section id="about">
-        <AboutSection />
-      </section>
-
+      <Hero />
+      <CategoryGrid />
+      <AboutSection />
       <ProductDetailSection />
-
-      <section id="quality">
-        <QualitySection />
-      </section>
-
-      <section id="sustainability">
-        <SustainabilitySection />
-      </section>
-
-      <section id="compliance">
-        <ComplianceSection />
-      </section>
-
+      <QualitySection />
+      <SustainabilitySection />
+      <ComplianceSection />
       <CatalogSection />
-
-      <section id="contact">
-        <ContactSection />
-      </section>
+      <ContactSection />
     </>
   );
 }
